9638e6a88cf02cae27117aa9fe3e424c34b994bb
Mustafa Eren Alkan <mustafaerenalkan@Mustafa-MacBook-Pro.local>
2025-07-11 01:12:10 +0300

    fix: PWA yapılandırmasını next.config.ts içinde güncelle

--- astro-web/next.config.ts
+++ astro-web/next.config.ts
@@ -1,31 +1,16 @@
-// astro-web/next.config.ts
-import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = withPWA({
-  reactStrictMode: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-    domains: ['picsum.photos', 'localhost', '192.168.1.103'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/robots.txt',
-        destination: '/api/robots',
-      },
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap.xml',
-      },
-    ];
-  },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // develop'da cache kapalı
 });
 
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['picsum.photos', 'localhost', '192.168.1.103'],
+  },
+});
